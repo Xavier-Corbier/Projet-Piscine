d35b57d37e26f0d59fc2be6e06ce6114bd56ddfe
Margot <margot-georget@hotmail.fr>
2020-12-01 22:54:55 +0100

    addStudent

--- backend/server/routes/private/student/addStudent.js
+++ backend/server/routes/private/student/addStudent.js
@@ -1,3 +1,10 @@
-const Student = require('');
+const Student = require('../../../models/student');
 
-app.post('/api/')
+app.post('/', (req, res, next) => {
+    const student = new Student({
+        ...req.body
+    });
+    student.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+        .catch(error => res.status(400).json({ error }));
+})
