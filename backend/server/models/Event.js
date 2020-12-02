3e06e6fd87a30f6c6d9cf2caf75bfbcaa3c3a3d9
Margot <margot-georget@hotmail.fr>
2020-12-02 08:02:14 +0100

    type Date add

--- backend/server/models/Event.js
+++ backend/server/models/Event.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const eventSchema = mongoose.Schema({
     wording: { type: String, required: true },
     durationSlot: { type: Int, required: true },
-    deadline: { type: Int, required: true },
+    deadline: { type: Date, required: true },
     nbMaxStudent: { type: Int, required: true },
     nbJury: { type: Int, required: true },
-    startdate: { type: Int, required: true },
-    endDate: { type: Int, required: true },
+    startdate: { type: Date, required: true },
+    endDate: { type: Date, required: true },
     promo: { type: String, required: true },
     listSlots: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Slot', required: true }]
 });
