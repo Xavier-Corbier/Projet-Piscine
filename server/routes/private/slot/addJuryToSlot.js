997c0ee6777434c763ae5b82c79cd1e6a57de946
Margot <margot-georget@hotmail.fr>
2021-01-13 23:55:58 +0100

    errorAddJury

--- server/routes/private/slot/addJuryToSlot.js
+++ server/routes/private/slot/addJuryToSlot.js
@@ -7,10 +7,11 @@ module.exports = async (req, res, next) => {
         const teacherId = req.query.idTeacher;
         const slotId = req.query.idSlot;
 
-        if (await teacherController.getTeacherId(teacherId) === undefined) {
+        console.log(teacherId)
+        if (await teacherController.getTeacherById(teacherId) === undefined) {
             return res.status(400).json({ error: 'Le teacher spécifié n\'existe pas.' });
         }
-
+        console.log(slotId)
         const slot = await slotController.addJuryToSlot(slotId, teacherId);
 
         if (slot === undefined) {
@@ -21,6 +22,7 @@ module.exports = async (req, res, next) => {
         await slotController.addJuryToSlot(slotId, teacherId);
         await teacherController.addSlotToTeacher(teacherId, slotId);
 
+        return res.status(200).json({message: "Jury ajouté"});
 
     }catch(error){
         console.log(error.message);
