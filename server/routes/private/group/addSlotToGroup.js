a185d5102d507d64b2d34b26ad166eaa700e3ca2
Margot <margot-georget@hotmail.fr>
2021-01-14 16:36:15 +0100

    problem getStudentByPromo

--- server/routes/private/group/addSlotToGroup.js
+++ server/routes/private/group/addSlotToGroup.js
@@ -102,7 +102,7 @@ module.exports = async (req, res, next) => {
         return res.status(200).json(groupUpdate);
 
     }catch(error){
-        console.log(error.message);
+        console.log(error);
         return res.status(500).json({error: "Impossible d'ajouter ce slot au groupe"});
     }
 }
